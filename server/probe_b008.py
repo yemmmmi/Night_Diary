9dcce583c860575a3d9125bbc6e374bc9f6f6c1e
yemi <18016228639@163.com>
2026-08-25 12:03:12 +0800

    chore: remove debug probe file accidentally committed

--- server/probe_b008.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from datetime import date
-from typing import Annotated
-
-from fastapi import Query
-
-
-def p_annotated(p: Annotated[date | None, Query()] = None) -> None: ...
